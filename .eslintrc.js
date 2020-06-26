870ab2ccf7e63a9fcbf9fd843c3ff3dcfd0045b5
YUN-RU-TSENG <coco002197@gmail.com>
2020-06-26 12:12:42 +0800

    yarn add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "plugin:vue/essential",
+        "airbnb-base"
+    ],
+    "parserOptions": {
+        "ecmaVersion": 10,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+    }
+};
